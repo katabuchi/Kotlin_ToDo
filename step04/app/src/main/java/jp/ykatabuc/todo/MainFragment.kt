fb161a1f39dd2919f67cb45cb23c714d4fe98629
ykatabuc <ykatabuc@yahoo-corp.jp>
2019-12-19 09:42:44 +0900

    レイアウト調整

--- step04/app/src/main/java/jp/ykatabuc/todo/MainFragment.kt
+++ step04/app/src/main/java/jp/ykatabuc/todo/MainFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DividerItemDecoration
 import kotlinx.android.synthetic.main.fragment_main.*
 
 class MainFragment : Fragment() {
@@ -34,6 +35,8 @@ class MainFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         fragment_todo_rv.adapter = adapter
+        fragment_todo_rv.addItemDecoration(
+            DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL))
 
         viewModel.todoList.observe(viewLifecycleOwner, Observer {
             adapter.updateItems(it)
